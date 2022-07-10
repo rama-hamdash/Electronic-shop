8d4052d5dfb00e8d0874f022cf10afa4620d0561
Laolao <leenallaf98@gmail.com>
2022-07-11 00:20:37 +0300

    edit product fixed

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -38,4 +38,8 @@ class Product extends Model
     {
         return $this->belongsToMany(Order::class);
     }
+    public function model()
+    {
+        return $this->belongsTo(Modele::class);
+    }
 }
