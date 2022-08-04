18b00f9ece760c9c374abb77e7357207c77bb423
Laolao <leenallaf98@gmail.com>
2022-08-04 19:51:46 +0300

    switch photos in product fixed

--- app/Http/Livewire/Product.php
+++ app/Http/Livewire/Product.php
@@ -37,7 +37,7 @@ class Product extends Component
         }
         $this->sizes = array_unique($s);
     }
-  // TODO:: make colors 
+    // TODO:: make colors 
     public function getColors()
     {
         $c = [];
@@ -46,10 +46,11 @@ class Product extends Component
         }
         $this->colors = array_unique($c);
     }
-  
+
     public function selectProduct($id)
     {
         $this->product = $this->model->products->where('id', $id)->first();
+        $this->getColors();
     }
 
     public function render()
@@ -61,40 +62,40 @@ class Product extends Component
     {
 
         if (!Auth::check() == 'null') {
-        
+
             $this->alert('error', 'you have to login first...');
-        } else{
-        $product = modelProduct::whereId($id)->firstOrFail();
-        $duplicates = Cart::instance('default')->search(function ($cartItem, $rowId) use ($product) {
-            return $cartItem->id === $product->id;
-        });
-        if ($duplicates->isNotEmpty()) {
-            $this->alert('error', 'Product already exist!');
         } else {
-            Cart::instance('default')->add($product->id, $product->name, 1, $product->price)->associate(modelProduct::class);
-            $this->emit('updateCart');
-            $this->alert('success', 'Product added in your cart successfully.');
+            $product = modelProduct::whereId($id)->firstOrFail();
+            $duplicates = Cart::instance('default')->search(function ($cartItem, $rowId) use ($product) {
+                return $cartItem->id === $product->id;
+            });
+            if ($duplicates->isNotEmpty()) {
+                $this->alert('error', 'Product already exist!');
+            } else {
+                Cart::instance('default')->add($product->id, $product->name, 1, $product->price)->associate(modelProduct::class);
+                $this->emit('updateCart');
+                $this->alert('success', 'Product added in your cart successfully.');
+            }
         }
     }
-}
 
     public function addToWishList($id)
     {
         if (!Auth::check() == 'null') {
-        
+
             $this->alert('error', 'you have to login first...');
-        } else{
-        $product = modelProduct::whereId($id)->firstOrFail();
-        $duplicates = Cart::instance('wishlist')->search(function ($cartItem, $rowId) use ($product) {
-            return $cartItem->id === $product->id;
-        });
-        if ($duplicates->isNotEmpty()) {
-            $this->alert('error', 'Product already exist!');
         } else {
-            Cart::instance('wishlist')->add($product->id, $product->name, 1, $product->price)->associate(modelProduct::class);
-            $this->emit('updateCart');
-            $this->alert('success', 'Product added in your wishlist cart successfully.');
+            $product = modelProduct::whereId($id)->firstOrFail();
+            $duplicates = Cart::instance('wishlist')->search(function ($cartItem, $rowId) use ($product) {
+                return $cartItem->id === $product->id;
+            });
+            if ($duplicates->isNotEmpty()) {
+                $this->alert('error', 'Product already exist!');
+            } else {
+                Cart::instance('wishlist')->add($product->id, $product->name, 1, $product->price)->associate(modelProduct::class);
+                $this->emit('updateCart');
+                $this->alert('success', 'Product added in your wishlist cart successfully.');
+            }
         }
     }
-}
 }
